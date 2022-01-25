0f952390b6a3225f32bda6cc6c4373bedbdda234
Herig Recalde <herig14@gmail.com>
2022-01-25 16:44:00 -0500

    Bookbrary

---
+++ config/config.js
@@ -0,0 +1,5 @@
+//puerto
+process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+process.env.urldb ="Insertar cadena de conexión de base de datos MongoDB"
